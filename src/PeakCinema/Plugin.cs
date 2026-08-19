55c8df0e7df279954564513c2460f2c9f88ffcbe
megalon <megalongaming@gmail.com>
2026-08-19 09:44:39 -0400

    Add configs for cam reset key and for hold time.

--- src/PeakCinema/Plugin.cs
+++ src/PeakCinema/Plugin.cs
@@ -26,7 +26,6 @@ public partial class Plugin : BaseUnityPlugin
     internal static bool CameraWasSpawned { get; private set; }
     internal static bool Smoothing { get; private set; } = true;
     internal static float HoldTimer { get; private set; }
-    internal static float InitHoldTimer { get; private set; } = 1.5f;
     internal static List<VoiceObscuranceFilter> VoiceFilters = new List<VoiceObscuranceFilter>();
     internal static Vector3 DeathLocation { get; private set; }
     internal static bool PlayerVisibilityToggled { get; private set; } = false;
@@ -42,7 +41,7 @@ public partial class Plugin : BaseUnityPlugin
 
         ModConfig = new PluginModConfig(Config);
 
-        HoldTimer = InitHoldTimer;
+        HoldTimer = ModConfig.initHoldTimer.Value;
     }
 
     [HarmonyPatch(typeof(VoiceObscuranceFilter), "Start")]
@@ -73,7 +72,7 @@ public partial class Plugin : BaseUnityPlugin
         }
 
         CameraWasSpawned = false;
-        HoldTimer = InitHoldTimer;
+        HoldTimer = ModConfig.initHoldTimer.Value;
         DeathLocation = Vector3.zero;
 
         if (HUD == null)
@@ -112,17 +111,29 @@ public partial class Plugin : BaseUnityPlugin
 
             ApplyPlayerVisibility(false);
         }
-        else if (Input.GetKey(ModConfig.toggleCinemaCamControlKey.Value))
+        
+        if (ModConfig.initHoldTimer.Value > 0)
         {
-            HoldTimer -= Time.deltaTime;
-            if (HoldTimer <= 0)
+            if (Input.GetKey(ModConfig.resetCinemaCamKey.Value))
+            {
+                HoldTimer -= Time.deltaTime;
+                if (HoldTimer <= 0)
+                {
+                    MoveCameraToPlayerPosition(__instance);
+                    if (!__instance.on) __instance.on = true;
+                    HoldTimer = -1;
+                }
+            }
+        } else
+        {
+            if (Input.GetKeyUp(ModConfig.resetCinemaCamKey.Value))
             {
                 MoveCameraToPlayerPosition(__instance);
                 if (!__instance.on) __instance.on = true;
-                HoldTimer = -1;
             }
         }
-        else if (Input.GetKeyUp(ModConfig.toggleCinemaCamControlKey.Value))
+
+        if (Input.GetKeyUp(ModConfig.toggleCinemaCamControlKey.Value))
         {
             if (!CinemaCamActive)
             {
@@ -136,7 +147,7 @@ public partial class Plugin : BaseUnityPlugin
                 __instance.on = !__instance.on;
             }
 
-            HoldTimer = InitHoldTimer;
+            HoldTimer = ModConfig.initHoldTimer.Value;
         }
 
         if (__instance.on)
@@ -419,6 +430,8 @@ public partial class Plugin : BaseUnityPlugin
     public class PluginModConfig
     {
         public readonly ConfigEntry<KeyCode> toggleCinemaCamControlKey;
+        public readonly ConfigEntry<KeyCode> resetCinemaCamKey;
+        public readonly ConfigEntry<float> initHoldTimer;
         public readonly ConfigEntry<KeyCode> exitCinemaCamKey;
         public readonly ConfigEntry<KeyCode> keyTogglePlayer;
 
@@ -445,7 +458,9 @@ public partial class Plugin : BaseUnityPlugin
         public PluginModConfig(ConfigFile config)
         {
             // General
-            toggleCinemaCamControlKey = config.Bind<KeyCode>("General", "Toggle Cinema Cam Control", KeyCode.F3, "Hold F3 to reset camera position to player, press F3 to toggle on/off.");
+            toggleCinemaCamControlKey = config.Bind<KeyCode>("General", "Toggle Cinema Cam Control", KeyCode.F3, "Press to toggle Cinema Cam on/off.");
+            resetCinemaCamKey = config.Bind<KeyCode>("General", "Reset Cinema Cam Position", KeyCode.F3, "Hold to reset camera position to the player position.");
+            initHoldTimer = config.Bind<float>("General", "Reset Hold Time", 1.5f, "Time in seconds to hold the camera reset key before it resets. Default 1.5, but can be 0!");
             exitCinemaCamKey = config.Bind<KeyCode>("General", "Exit Cinema Cam", KeyCode.Escape, "Exits the cinema camera and re-enables player input.");
             keyTogglePlayer = config.Bind<KeyCode>("General", "Toggle Player Visibility", KeyCode.F4, "Toggles your character model (body/head/cosmetics) on/off.");
 
